984ddd5646d771ffbc77949610bbbd3bd146c369
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-09-04 20:22:29 +0800

    sample_file.sh

---
+++ sample_files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#this script sample 100 .jpg, .xml files from JPEGImages and Annotations separately(VOC directory structure)
+
+ls JPEGImages > fnames.txt
+if [ -d sample ]; then
+    rm -r sample
+fi
+mkdir sample
+sed -i 's/.jpg//g' fnames.txt
+shuf -n 100 fnames.txt -o fnames.txt
+for fname in $(cat fnames.txt)
+do
+    cp -r $(find . -name "*$fname*") sample
+done
+rm fnames.txt
+tar -czf sample.tar.gz sample
