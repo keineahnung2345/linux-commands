570f0a3fad370b33641de07cef99391e3200519c
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-12-17 11:14:18 +0800

    check_list_include_item.sh

---
+++ check_list_include_item.sh
@@ -0,0 +1,13 @@
+function list_include_item {
+  local list="$1"
+  local item="$2"
+  if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]] ; then
+    # yes, list include item
+    result=0
+  else
+    result=1
+  fi
+  return $result
+}
+
+`list_include_item "10 11 12" "11"`  && echo "yes" || echo "no"
